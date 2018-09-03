efb37c275d724173c50a428cccd26e896d139d53
maniezhilan <maniezhilan@gmail.com>
2018-09-03 16:17:09 -0500

    New design

--- public/index.js
+++ public/index.js
@@ -1,28 +1,41 @@
 
 
+
 var explosionWords = ['Pop!', 'Snap!', 'Crack!'];
 var bubblesPopped = 0;
 var $score = document.getElementById('score');
-var $pause = document.getElementById('btnPause');
+var $container = $('.container');
+var $speed = 10000;
+
 
 function updateScore(offset) {
     bubblesPopped += offset;
     $score.innerHTML = 'Popped ' + bubblesPopped + ' bubbles!';
 }
 
-function pauseAll(){
-	console.log('pause');
-	$pause.click(function() {
+ $('#btnPlay').click(function(e) {
+    	$('bg-overlay').fadeToggle("fast");
+    	timeline.play();
+    });
+    $('#btnPause').click(function(e) {
+    	timeline.pause();
+    });
+    $('#btnRestart').click(function(e) {
+    	timeline.play();
+    });
 
-    	$( "bubble" ).each(function( index ) {
-  			console.log( index + ": " + $( this ).pause());
-		});
-	});
-}
+
 function createBubble() {
     // create bubble graphic
     var $bubble = document.createElement('div');
     $bubble.classList.add('bubble');
+    const start = 10;
+	const end = 100;
+	const unit = undefined;
+	const onlyWholeNumbers = true;
+	var size = just.random(start, end, unit, onlyWholeNumbers);
+    $bubble.style.height = size;
+    $bubble.style.width = size;
     // wrap in a larger div so bubbles are easy to pop while moving
     var $boundingBox = document.createElement('div');
     $boundingBox.classList.add('bubble-wrap');
@@ -57,14 +70,15 @@ function createExplosion(x, y) {
 }
 function destroyBubble($bubble) {
     return function () {
-        // create explosion at bubbles old position
-        var rect = $bubble.getBoundingClientRect();
-        var centerX = (rect.right - rect.left) * .45 + rect.left;
-        var centerY = (rect.bottom - rect.top) * .45 + rect.top;
-        createExplosion(centerX, centerY);
-        updateScore(1);
-        // remove bubble
-        $bubble.style.display = 'none';
+
+	        // create explosion at bubbles old position
+	        var rect = $bubble.getBoundingClientRect();
+	        var centerX = (rect.right - rect.left) * .45 + rect.left;
+	        var centerY = (rect.bottom - rect.top) * .45 + rect.top;
+	        createExplosion(centerX, centerY);
+	        updateScore(1);
+	        // remove bubble
+	        $bubble.style.display = 'none';
     };
 }
 function generateBubbles(min, max) {
@@ -76,32 +90,39 @@ function generateBubbles(min, max) {
     return targets;
 }
 function animateBubbles() {
+
     var bubbles = generateBubbles(12, 20);
-    var player = just.animate({
-        targets: bubbles,
-        to: '5s',
-        easing: 'ease-in',
-        css: {
-            transform: function () {
-                var endTranslateY = just.random(100, 110, 'vh', true);
-                var startScale = just.random(80, 100, null, true);
-                var endScale = just.random(40, 80, null, true);
-                return [
-                    'translateY(0) scale(0.' + startScale + ')',
-                    'translatey(-' + endTranslateY + ') scale(0.' + endScale + ')'
-                ];
-            }
-        },
-        delay: function () {
-            return just.random(0, 10000);
-        }
-    })
-        .on('finish', function () {
-        bubbles.forEach(function (bubble) {
-            document.body.removeChild(bubble);
-        });
+    $(".dial").knob({
+        'release' : function (value) {
+        	$speed = value * 1000
+       }
+
+	});
+    var endTranslateY = just.random(100, 110, 'vh', true);
+    var startScale = just.random(50, 100, null, true);
+    var endScale = just.random(10, 80, null, true);
+    const timeline = just.animate({
+	    targets: bubbles,
+	    duration: 5000,
+	    easing: 'ease-in',
+	    web: {
+         	transform: [
+                 	'translatey(-' + endTranslateY + ') scale(0.' + endScale + ')',
+                     'translateY(0) scale(0.' + startScale + ')'
+                 ]
+	    }
+	})
+
+    timeline.on('finish', function () {
+	    bubbles.forEach(function (bubble) {
+	        document.body.removeChild(bubble);
+	    })
     });
-    return player;
+
+    timeline.play();
+
+
+    return timeline;
 }
 function startBubbles() {
     animateBubbles().on('finish', startBubbles);
@@ -109,9 +130,9 @@ function startBubbles() {
 function startWaves() {
     just.animate({
         targets: document.body,
-        to: '10s',
+        duration: 10000,
         fill: 'both',
-        css: {
+        web: {
             backgroundColor: [
                 'hsl(196, 92.2%, 20%)',
                 'hsl(196, 92.2%, 15%)'
@@ -119,7 +140,7 @@ function startWaves() {
         },
         direction: 'alternate',
         iterations: Infinity
-    });
+    }).play();
 }
 startBubbles();
 startWaves();
